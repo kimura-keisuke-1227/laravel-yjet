58891274cd394865c1a590fc99cd2e4004815ffa
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-22 10:58:44 +0900

    とりあえずタスク登録画面

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
     <section class="py-8">
         <div class="container px-4 mx-auto">
             <div class="py-4 bg-white rounded">
-                <form action="{{ Route('project.store') }}" method="post" enctype="multipart/form-data">
+                <form action="{{ Route('task.store') }}" method="post" enctype="multipart/form-data">
                     <!-- ▼▼▼▼エラーメッセージ▼▼▼▼　-->
                     @if ($errors->any())
                         <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
@@ -17,16 +17,29 @@
                     @endif
                     <!-- ▲▲▲▲エラーメッセージ▲▲▲▲　-->
                     @csrf
+
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title">プロジェクト名</label>
-                        <input id="project_name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="text" name="project_name">
+                        <div class="flex">
+                            <select id="chemical_id" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="chemical_id">
+                                <option value="0">プロジェクトを選択してください。</option>
+                                @foreach ($projects as $project)
+                                <option value="{{$project -> id}}">                                >
+                                    {{$project->project}}
+                                </option>
+                                @endforeach
+                            </select>
+                            <div class="pointer-events-none transform -translate-x-full flex items-center px-2 text-gray-500">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20">
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
+                                </svg>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title"> 開始日</label>
-                        <input id="start_date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="date" name="start_date">
+                        <label class="block text-sm font-medium mb-2" for="title">タスク名</label>
+                        <input id="project_name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
+                            type="text" name="task_name">
                     </div>
 
                     <div class="flex px-6 pb-4 border-b">
