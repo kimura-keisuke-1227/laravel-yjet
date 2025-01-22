78d9ad3f03c7a5dd497f85185484a4c88d74f8a6
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-22 10:09:27 +0900

    プロジェクトのルート設定

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WorkController;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('work', WorkController::class);
+Route::resource('project', ProjectController::class);
 
 
 Route::get('/weekly',[WorkController::class,'weekly'])
