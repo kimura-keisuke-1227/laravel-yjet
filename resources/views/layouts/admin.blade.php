e5c36c9389c62b27b4a3d70ea00cccdc96a6fbfc
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-22 12:55:20 +0900

    画面表示修正

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,14 +5,19 @@
     <title>管理画面</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap">
-    <link rel="stylesheet" href="/css/admin/tailwind/tailwind.min.css">
-    <link rel="stylesheet" href="/css/admin/select2.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap">
+    <link rel="stylesheet" href="{{ asset('/css/admin/tailwind/tailwind.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/admin/select2.min.css')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
-    <script src="/js/main.js"></script>
-    <script src="/js/admin/jquery-3.6.0.slim.min.js"></script>
-    <script src="/js/admin/select2.min.js"></script>
+    <script src="{{ asset('/js/main.js')}}"></script>
+    <script src="{{ asset('/js/admin/jquery-3.6.0.slim.min.js')}}"></script>
+    <script src="{{ asset('/js/admin/select2.min.js')}}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/style.css')}}" >
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/css/theme.default.min.css">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
 </head>
 
 <body class="antialiased bg-body text-body font-body">
@@ -58,7 +63,7 @@
                         </li>
                         <li>
                             <a class="flex items-center pl-3 py-3 pr-4 text-gray-50 hover:bg-gray-900 rounded"
-                                href="/admin/blogs">
+                                href="{{Route('subcontractor.index')}}">
                                 <span class="inline-block mr-3">
                                     <svg class="text-gray-600 w-5 h-5" viewbox="0 0 18 10" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -67,7 +72,7 @@
                                             fill="currentColor"></path>
                                     </svg>
                                 </span>
-                                <span>ブログ管理</span>
+                                <span>発注先管理</span>
                             </a>
                         </li>
                         <li>
@@ -165,12 +170,17 @@
                     <!-- ▲▲▲▲登録完了メッセージ▲▲▲▲　-->
                 @endif
 
-                <!-- ▼▼▼▼ページ毎の個別内容▼▼▼▼　-->
-                @yield('content')
-                <!-- ▲▲▲▲ページ毎の個別内容▲▲▲▲　-->
+                <div class="pc_container ml-80">
+                    @yield('content')
+                </div>
             </main>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
