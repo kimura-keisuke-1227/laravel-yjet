06295b699304d21a4b41d514e5d41b4e3be7c357
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-22 10:51:44 +0900

    タスク画面

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('title', 'プロジェクト')
+
+@section('content')
+    <div class="container">
+
+        <table class='table table-striped'>
+            <tr>
+                <th>ID</th>
+                <th>プロジェクト名</th>
+                <th>開始日</th>
+                <th>終了日</th>
+                <th></th>
+                <th></th>
+            </tr>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project -> id}}</td>
+                    <td>{{ $project -> project_name }}</td>
+                    <td>{{ $project -> start_date }}</td>
+                    <td>{{ $project -> end_date }}</td>
+                    <td><td><a href="{{Route('project.edit',[ 'project' => $project -> id])}}">詳細・修正</a></td></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+@endsection
