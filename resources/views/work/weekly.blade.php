d4320d6a4882c68631add8bfca6c670c483616ce
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-25 13:32:55 +0900

    集計範囲の指定を可能に

--- resources/views/work/weekly.blade.php
+++ resources/views/work/weekly.blade.php
@@ -2,16 +2,29 @@
 @section('content')
     <div class="container">
     <table class='table table-striped'>
-        <tr>
-            <td>
-                <input id="date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="date" name="date" ></td>
-            </td>
-            <td>
-                <input id="scheduled_time" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="scheduled_time" value="">
-            </td>
-        </tr>
+    <form action="{{ Route('weekly.with_date')}}" method="POST"
+    enctype="multipart/form-data">
+            @csrf
+            <tr>
+                <td>
+                    <input id="base_date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="date" name="base_date" value="{{$base_date}}"></td>
+                </td>
+                <td>
+                    <input id="days_before" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="days_before" value="{{$days}}">日間
+                </td>
+                <td>
+                    <div class="flex px-6 pb-4 border-b">
+                        <div class="ml-auto">
+                            <button type="submit"
+                                class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">表示</button>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </form>
     </table>
-
+        <br>
+        <br>
         <table class='table table-striped'>
             <tr>
                 <th>外注先</th>
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::get('work_copy/{work}',[WorkController::class,'copy_work'])->name('work.c
 
 Route::put('project/multipleWorkUpdate/{project}', [WorkController::class,'multipleUpdate'])->name('work.multipleUpdate');
 
-Route::get('/weekly',[WorkController::class,'weekly'])
-->name('weekly');
+Route::get('/weekly',[WorkController::class,'weekly'])->name('weekly');;
+Route::post('/weekly_with_base_date',[WorkController::class,'weekly_with_base_date'])->name('weekly.with_date');;
+
