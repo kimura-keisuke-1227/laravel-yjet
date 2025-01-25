db75fa3e6c7f2f10c744b46fd91ad01915c1f5c8
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-25 13:54:10 +0900

    外注先編集画面

--- resources/views/subcontractor/edit.blade.php
+++ resources/views/subcontractor/edit.blade.php
@@ -4,9 +4,7 @@
     <section class="py-8">
         <div class="container px-4 mx-auto">
             <div class="py-4 bg-white rounded">
-                <form action="{{ Route('project.update',[ 'project' => $project]) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
+                <form action="{{ Route('subcontractor.store') }}" method="post" enctype="multipart/form-data">
                     <!-- ▼▼▼▼エラーメッセージ▼▼▼▼　-->
                     @if ($errors->any())
                         <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
@@ -18,26 +16,26 @@
                         </div>
                     @endif
                     <!-- ▲▲▲▲エラーメッセージ▲▲▲▲　-->
+                    @csrf
 
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title">プロジェクト名</label>
-                        <input id="project_name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="text" name="project_name" value="{{$project->project_name}}">
-                    </div>
-
-                    <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title"> 開始日</label>
-                        <input id="start_date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="date" name="start_date"  value="{{$project->start_date}}">
+                        <label class="block text-sm font-medium mb-2" for="title">外注先名称</label>
+                        <input id="subcontractor_name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
+                            type="text" name="subcontractor_name" value="{{$subcontractor->name}}">
                     </div>
 
 
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title"> 終了日</label>
-                        <input id="end_date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="date" name="end_date"  value="{{$project->end_date}}">
+                        <label class="block text-sm font-medium mb-2" for="title">外注先コード</label>
+                        <input id="subcontractor_code" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
+                            type="text" name="subcontractor_code" value="{{$subcontractor->code}}">
                     </div>
 
+                    <div class="mb-6">
+                        <label class="block text-sm font-medium mb-2" for="title">外注先略称</label>
+                        <input id="subcontractor_abbreviation" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
+                            type="text" name="subcontractor_abbreviation" value="{{$subcontractor->abbreviation}}">
+                    </div>
                     <div class="flex px-6 pb-4 border-b">
                         <div class="ml-auto">
                             <button type="submit"
@@ -48,5 +46,14 @@
             </div>
         </div>
     </section>
+
+    <script>
+        // 現在の日付を取得してフォーマット
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0]; // YYYY-MM-DD形式に変換
+
+        // input要素に設定
+        document.getElementById('start_date').value = formattedDate;
+    </script>
 
 @endsection
