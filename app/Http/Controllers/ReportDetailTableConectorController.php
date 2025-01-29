20e2a0be61a9ba35d9bdf77abfe4f4f07623bb16
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 17:43:55 +0900

    テーブルを結合するコネクタ

---
+++ app/Http/Controllers/ReportDetailTableConectorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReportDetailTableConectorRequest;
+use App\Http\Requests\UpdateReportDetailTableConectorRequest;
+use App\Models\ReportDetailTableConector;
+
+class ReportDetailTableConectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReportDetailTableConectorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReportDetailTableConector $reportDetailTableConector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ReportDetailTableConector $reportDetailTableConector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReportDetailTableConectorRequest $request, ReportDetailTableConector $reportDetailTableConector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReportDetailTableConector $reportDetailTableConector)
+    {
+        //
+    }
+}
---
+++ app/Models/ReportDetailTableConector.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportDetailTableConector extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReportDetailTableConectorFactory> */
+    use HasFactory;
+}
