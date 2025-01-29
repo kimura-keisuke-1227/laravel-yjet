542d932ff39c8dc4e96f3497466720016298290d
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 17:48:14 +0900

    テーブル名、カラム名修正

--- app/Models/ReportDetailTableConector.php
+++ app/Models/ReportDetailTableConector.php
@@ -10,10 +10,10 @@ class ReportDetailTableConector extends Model
     /** @use HasFactory<\Database\Factories\ReportDetailTableConectorFactory> */
     use HasFactory;
 
-    const TABLE_NAME_OF_REPORT_TABLE_CONNECTOR = 'report_table_connector';
+    const TABLE_NAME_OF_REPORT_TABLE_CONNECTOR = 'report_table_connectors';
 
     const CLM_NAME_OF_ID = 'id';
-    const CLM_NAME_OF_REPORT_HEADER_ID = 'repot_header_id';
+    const CLM_NAME_OF_REPORT_HEADER_ID = 'report_header_id';
     const CLM_NAME_OF_LEFT_COLUMN= 'left_column_id';
     const CLM_NAME_OF_RIGHT_COLUMN= 'right_column_id';
 
