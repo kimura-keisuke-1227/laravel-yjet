c4f9b6475ffab95f62cb8bd5b942d2fff4eaad79
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 16:36:06 +0900

    レポートヘッダーのモデル

--- app/Models/ReportHeader.php
+++ app/Models/ReportHeader.php
@@ -9,4 +9,18 @@ class ReportHeader extends Model
 {
     /** @use HasFactory<\Database\Factories\ReportHeaderFactory> */
     use HasFactory;
+
+    const TABLE_NAME_OF_REPORT = 'reports';
+
+    const CLM_NAME_OF_ID = 'id';
+    const CLM_NAME_OF_REPORT_CODE = 'report_code';
+    const CLM_NAME_OF_REPORT_NAME = 'report_name';
+    const CLM_NAME_OF_IS_ACTIVE = 'is_active';
+
+    protected $fillable = [
+        self::CLM_NAME_OF_REPORT_CODE,
+        self::CLM_NAME_OF_REPORT_NAME,
+        self::CLM_NAME_OF_IS_ACTIVE,
+    ];
+
 }
