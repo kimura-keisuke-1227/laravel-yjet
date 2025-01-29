2dada47eeb717285ee48f7c45fe43cb1101b5d7c
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 17:22:55 +0900

    テーブルのDB設計

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -9,4 +9,16 @@ class Table extends Model
 {
     /** @use HasFactory<\Database\Factories\TableFactory> */
     use HasFactory;
+
+    const TABLE_NAME_OF_TABLES = 'tables';
+
+    const CLM_NAME_OF_TABLE_NAME = 'table_name';
+    const CLM_NAME_OF_TABLE_DISPLAY_NAME = 'table_text_name';
+    const CLM_NAME_OF_REMARK = 'remark';
+
+    protected $fillable = [
+        self::CLM_NAME_OF_TABLE_NAME,
+        self::CLM_NAME_OF_TABLE_DISPLAY_NAME,
+        self::CLM_NAME_OF_REMARK,
+    ];
 }
--- database/migrations/2025_01_29_081613_create_tables_table.php
+++ database/migrations/2025_01_29_081613_create_tables_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Table as Table;
+
 return new class extends Migration
 {
     /**
@@ -11,8 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tables', function (Blueprint $table) {
+        Schema::create(Table::TABLE_NAME_OF_TABLES, function (Blueprint $table) {
             $table->id();
+            $table->string(Table::CLM_NAME_OF_TABLE_NAME);
+            $table->string(Table::CLM_NAME_OF_TABLE_DISPLAY_NAME);
+            $table->text(Table::CLM_NAME_OF_REMARK);
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tables');
+        Schema::dropIfExists(Table::TABLE_NAME_OF_TABLES);
     }
 };
