816f47fc3a653f46e4ded466c3992aa0979fb926
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 16:31:56 +0900

    レポート用コントローラー作成

---
+++ app/Http/Controllers/ReportHeaderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReportHeaderRequest;
+use App\Http\Requests\UpdateReportHeaderRequest;
+use App\Models\ReportHeader;
+
+class ReportHeaderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReportHeaderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReportHeader $reportHeader)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ReportHeader $reportHeader)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReportHeaderRequest $request, ReportHeader $reportHeader)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReportHeader $reportHeader)
+    {
+        //
+    }
+}
---
+++ app/Models/ReportHeader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportHeader extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReportHeaderFactory> */
+    use HasFactory;
+}
