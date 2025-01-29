5f2e38732cee6e97f3a178d8bf1540fe9637b169
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-29 17:25:30 +0900

    COLUMNのモデル設計

--- app/Models/Column.php
+++ app/Models/Column.php
@@ -9,4 +9,19 @@ class Column extends Model
 {
     /** @use HasFactory<\Database\Factories\ColumnFactory> */
     use HasFactory;
+
+
+    const COLUMN_NAME_OF_COLUMNS = 'columns';
+
+    const CLM_NAME_OF_TABLE_ID = 'table_id';
+    const CLM_NAME_OF_COLUMN_NAME = 'columns_name';
+    const CLM_NAME_OF_COLUMN_DISPLAY_NAME = 'columns_text_name';
+    const CLM_NAME_OF_REMARK = 'remark';
+
+    protected $fillable = [
+        self::CLM_NAME_OF_TABLE_ID,
+        self::CLM_NAME_OF_COLUMN_NAME,
+        self::CLM_NAME_OF_COLUMN_DISPLAY_NAME,
+        self::CLM_NAME_OF_REMARK,
+    ];
 }
