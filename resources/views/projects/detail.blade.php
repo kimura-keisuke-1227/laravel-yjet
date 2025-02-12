7d1696eb576e1bc96d5ecffb407b1b95854df5d7
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-02-12 13:23:54 +0900

    プロジェクト検索フォーム

--- resources/views/projects/detail.blade.php
+++ resources/views/projects/detail.blade.php
@@ -24,38 +24,41 @@
                             type="text" name="project_name">
                     </div>
                     <label class="block text-sm font-medium mb-2" for="user_id">担当者</label>
-                    <select id="user_id" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="user_id">
+                    <select id="user_id" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded"
+                        name="user_id">
                         <option value="0">未選択</option>
                         @foreach ($users as $user)
-                        <option value="{{$user -> id}}">
-                            {{$user->name}}
-                        </option>
+                            <option value="{{ $user->id }}">
+                                {{ $user->name }}
+                            </option>
                         @endforeach
                     </select>
                     <label class="block text-sm font-medium mb-2" for="customer_id">顧客</label>
-                    <select id="customer_id" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="customer_id">
+                    <select id="customer_id"
+                        class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded"
+                        name="customer_id">
                         <option value="0">未選択</option>
                         @foreach ($customers as $customer)
-                        <option value="{{$customer -> id}}">
-                            {{$customer->customer_code}}_{{$customer->customer_name}}
-                        </option>
+                            <option value="{{ $customer->id }}">
+                                {{ $customer->customer_code }}_{{ $customer->customer_name }}
+                            </option>
                         @endforeach
                     </select>
 
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title">金額</label>
-                        <input id="amount" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="number" name="amount" value="0">
-                    </div>
-
-                    <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="title"> 開始日</label>
+                        <label class="block text-sm font-medium mb-2" for="search_date">日付</label>
+                        <select id="search_date"
+                            class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded"
+                            name="search_date">
+                            <option value="start_date">開始日</option>
+                            <option value="end_date" selected>終了日</option>
+                        </select>
                         <input id="start_date" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded"
-                            type="date" name="start_date"  max="2382-12-31">
+                            type="date" name="start_date" max="2382-12-31">
                     </div>
                     <div class="mb-6">
                         <label class="block text-sm font-medium mb-2" for="title">メモ</label>
-                        <textarea  rea id="remark" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" name="remark" rows=10></textarea>
+                        <textarea rea id="remark" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" name="remark" rows=10></textarea>
                     </div>
 
                     <div class="flex px-6 pb-4 border-b">
